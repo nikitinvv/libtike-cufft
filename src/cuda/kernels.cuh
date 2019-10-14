086df8d6300f7ba3c5f3ff780196a2f4b268653d
Daniel Ching <carterbox@users.noreply.github.com>
2019-10-14 14:16:56 -0500

    REF: Remove redundant indexing expressions from kernels
    Because:
    - It reduces computation
    - Long indexing expressions make lines too wide

--- src/cuda/kernels.cuh
+++ src/cuda/kernels.cuh
@@ -3,150 +3,216 @@
 #define PI 3.1415926535
 
 // probe multiplication of the object array part
-void __global__ mulprobe(float2 *g, float2 *f, float2 *prb, float *scanx, float *scany,
-						 int Ntheta, int Nz, int N, int Nscan, int Nprb, int Ndetx, int Ndety)
+void __global__ mulprobe(float2 *g, float2 *f, float2 *prb,
+                         float *scanx, float *scany,
+                         int Ntheta, int Nz, int N, int Nscan, int Nprb,
+                         int Ndetx, int Ndety)
 {
-	int tx = blockDim.x * blockIdx.x + threadIdx.x;
-	int ty = blockDim.y * blockIdx.y + threadIdx.y;
-	int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  int tx = blockDim.x * blockIdx.x + threadIdx.x;
+  int ty = blockDim.y * blockIdx.y + threadIdx.y;
+  int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
+    return;
 
-	if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
-		return;
+  // coordinates in the probe array
+  int ix = tx % Nprb;
+  int iy = tx / Nprb;
+  // probe voxel for multiplication
+  float2 prb0 = prb[
+    + ix
+    + iy * Nprb
+    + tz * Nprb * Nprb
+  ];
 
-	// coordinates in the probe array
-	int ix = tx % Nprb;
-	int iy = tx / Nprb;
-	// shift in the object array to the starting point of probe multiplication
-	int shift = (Ndety - Nprb) / 2 * Ndetx + (Ndetx - Nprb) / 2;
+  // coordinates in the object array
+  size_t shift = (
+    // shift in the object array to the starting point of probe multiplication
+    + (Ndety - Nprb) / 2 * Ndetx
+    + (Ndetx - Nprb) / 2
+    // shift in the object array multilication for this thread
+    + ix
+    + iy * Ndetx
+    + ty * Ndetx * Ndety
+    + tz * Ndetx * Ndety * Nscan
+  );
+  // object voxel for multiplication
+  float2 g0 = g[shift];
 
-	float2 prb0 = prb[ix + iy * Nprb + tz * Nprb * Nprb];									  // probe voxel for multiplication
-	float2 g0 = g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan]; // object voxel for multiplication
-	float c = 1 / sqrtf(Ndetx * Ndety);														  //fft constant
-
-	// multilication in complex variables
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].x = c * prb0.x * g0.x - c * prb0.y * g0.y;
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].y = c * prb0.x * g0.y + c * prb0.y * g0.x;
+  // multiplication in complex variables
+  float c = 1 / sqrtf(Ndetx * Ndety); // fft constant
+  g[shift].x = c * (prb0.x * g0.x - prb0.y * g0.y);
+  g[shift].y = c * (prb0.x * g0.y + prb0.y * g0.x);
 }
 
 // adjoint probe multiplication of the object array part
 void __global__ mulaprobe(float2 *f, float2 *g, float2 *prb, float *scanx, float *scany,
 						  int Ntheta, int Nz, int N, int Nscan, int Nprb, int Ndetx, int Ndety)
 {
-	int tx = blockDim.x * blockIdx.x + threadIdx.x;
-	int ty = blockDim.y * blockIdx.y + threadIdx.y;
-	int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  int tx = blockDim.x * blockIdx.x + threadIdx.x;
+  int ty = blockDim.y * blockIdx.y + threadIdx.y;
+  int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
+    return;
 
-	if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
-		return;
+  // closest integers for scan positions
+  int stx = roundf(scanx[ty + tz * Nscan]);
+  int sty = roundf(scany[ty + tz * Nscan]);
+  // skip scans where the probe position is negative (undefined)
+  if (stx < 0 || sty < 0)
+    return;
 
-	// coordinates in the probe array
-	int ix = tx % Nprb;
-	int iy = tx / Nprb;
-	// closest integers for scan positions
-	int stx = roundf(scanx[ty + tz * Nscan]);
-	int sty = roundf(scany[ty + tz * Nscan]);
-	// skip scans where the probe position is negative (undefined)
-	if (stx < 0 || sty < 0)
-		return;
-	// shift in the object array to the starting point of probe multiplication
-	int shift = (Ndety - Nprb) / 2 * Ndetx + (Ndetx - Nprb) / 2;
+  // coordinates in the probe array
+  int ix = tx % Nprb;
+  int iy = tx / Nprb;
+  // probe voxel for multiplication
+  float2 prb0 = prb[
+    + ix
+    + iy * Nprb
+    + tz * Nprb * Nprb
+  ];
 
-	float2 g0 = g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan]; // data voxel for multiplication
-	float2 prb0 = prb[ix + iy * Nprb + tz * Nprb * Nprb];									  // probe voxel for multiplication
-	float c = 1 / sqrtf(Ndetx * Ndety);														  //fft constant
-	// multilication in complex variables with simultaneous writing to the object array
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].x = c * prb0.x * g0.x + c * prb0.y * g0.y;
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].y = c * prb0.x * g0.y - c * prb0.y * g0.x;
+  // coordinates in the object array
+  size_t shift = (
+    // shift in the object array to the starting point of probe multiplication
+    + (Ndety - Nprb) / 2 * Ndetx
+    + (Ndetx - Nprb) / 2
+    // shift in the object array multilication for this thread
+    + ix
+    + iy * Ndetx
+    + ty * Ndetx * Ndety
+    + tz * Ndetx * Ndety * Nscan
+  );
+  // data voxel for multiplication
+  float2 g0 = g[shift];
+
+  // multiplication in complex variables
+  float c = 1 / sqrtf(Ndetx * Ndety); // fft constant
+  g[shift].x = c * (prb0.x * g0.x + prb0.y * g0.y);
+  g[shift].y = c * (prb0.x * g0.y - prb0.y * g0.x);
 }
 
 // adjoint object part multiplication of the probe array
 void __global__ mulaobj(float2 *prb, float2 *g, float2 *f, float *scanx, float *scany,
 						int Ntheta, int Nz, int N, int Nscan, int Nprb, int Ndetx, int Ndety)
 {
-	int tx = blockDim.x * blockIdx.x + threadIdx.x;
-	int ty = blockDim.y * blockIdx.y + threadIdx.y;
-	int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  int tx = blockDim.x * blockIdx.x + threadIdx.x;
+  int ty = blockDim.y * blockIdx.y + threadIdx.y;
+  int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
+    return;
 
-	if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
-		return;
+  // closest integers for scan positions
+  int stx = roundf(scanx[ty + tz * Nscan]);
+  int sty = roundf(scany[ty + tz * Nscan]);
+  // skip scans where the probe position is negative (undefined)
+  if (stx < 0 || sty < 0)
+    return;
 
-	// coordinates in the probe array
-	int ix = tx % Nprb;
-	int iy = tx / Nprb;
-	// closest integers for scan positions
-	int stx = roundf(scanx[ty + tz * Nscan]);
-	int sty = roundf(scany[ty + tz * Nscan]);
-	// skip scans where the probe position is negative (undefined)
-	if (stx < 0 || sty < 0)
-		return;
-	// shift in the object array to the starting point of probe multiplication
-	int shift = (Ndety - Nprb) / 2 * Ndetx + (Ndetx - Nprb) / 2;
+  // coordinates in the probe array
+  int ix = tx % Nprb;
+  int iy = tx / Nprb;
+  // object voxel for multiplication
+  float2 f0 = f[
+    + (stx + ix)
+    + (sty + iy) * N
+    + tz * Nz * N
+  ];
 
-	float2 g0 = g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan]; // data voxel for multiplication
-	float2 f0 = f[(stx + ix) + (sty + iy) * N + tz * Nz * N];								  // object voxel for multiplication
-	float c = 1 / sqrtf(Ndetx * Ndety);														  //fft constant
-	// multilication in complex variables with simultaneous writing to the object array
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].x = c * f0.x * g0.x + c * f0.y * g0.y;
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].y = c * f0.x * g0.y - c * f0.y * g0.x;
+  // coordinates in the object array
+  size_t shift = (
+    // shift in the object array to the starting point of probe multiplication
+    + (Ndety - Nprb) / 2 * Ndetx
+    + (Ndetx - Nprb) / 2
+    // shift in the object array multilication for this thread
+    + ix
+    + iy * Ndetx
+    + ty * Ndetx * Ndety
+    + tz * Ndetx * Ndety * Nscan
+  );
+  // data voxel for multiplication
+  float2 g0 = g[shift];
+
+  // multiplication in complex variables
+  float c = 1 / sqrtf(Ndetx * Ndety); // fft constant
+  g[shift].x = c * (f0.x * g0.x + f0.y * g0.y);
+  g[shift].y = c * (f0.x * g0.y - f0.y * g0.x);
 }
 
 // take part of the object array
 void __global__ takepart(float2 *g, float2 *f, float2 *prb, float *scanx, float *scany,
 						 int Ntheta, int Nz, int N, int Nscan, int Nprb, int Ndetx, int Ndety)
 {
-	int tx = blockDim.x * blockIdx.x + threadIdx.x;
-	int ty = blockDim.y * blockIdx.y + threadIdx.y;
-	int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  int tx = blockDim.x * blockIdx.x + threadIdx.x;
+  int ty = blockDim.y * blockIdx.y + threadIdx.y;
+  int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
+  return;
 
-	if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
-		return;
+  // coordinates in the probe array
+  int ix = tx % Nprb;
+  int iy = tx / Nprb;
+  // closest integers for scan positions
+  int stx = roundf(scanx[ty + tz * Nscan]);
+  int sty = roundf(scany[ty + tz * Nscan]);
+  // skip scans where the probe position is negative (undefined)
+  if (stx < 0 || sty < 0)
+    return;
 
-	// coordinates in the probe array
-	int ix = tx % Nprb;
-	int iy = tx / Nprb;
-	// closest integers for scan positions
-	int stx = roundf(scanx[ty + tz * Nscan]);
-	int sty = roundf(scany[ty + tz * Nscan]);
-	// skip scans where the probe position is negative (undefined)
-	if (stx < 0 || sty < 0)
-		return;
-	// shift in the object array to the starting point of probe multiplication
-	int shift = (Ndety - Nprb) / 2 * Ndetx + (Ndetx - Nprb) / 2;
+  // take the part
+  float2 f0 = f[(stx + ix) + (sty + iy) * N + tz * Nz * N];
 
-	// take the part
-	float2 f0 = f[(stx + ix) + (sty + iy) * N + tz * Nz * N];
-
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].x = f0.x;
-	g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan].y = f0.y;
+  // coordinates in the object array
+  size_t shift = (
+    // shift in the object array to the starting point of probe multiplication
+    + (Ndety - Nprb) / 2 * Ndetx
+    + (Ndetx - Nprb) / 2
+    // shift in the object array multilication for this thread
+    + ix
+    + iy * Ndetx
+    + ty * Ndetx * Ndety
+    + tz * Ndetx * Ndety * Nscan
+  );
+  g[shift].x = f0.x;
+  g[shift].y = f0.y;
 }
 
 // simultaneous writing to the object array
 void __global__ setpartobj(float2 *f, float2 *g, float2 *prb, float *scanx, float *scany,
 						   int Ntheta, int Nz, int N, int Nscan, int Nprb, int Ndetx, int Ndety)
 {
-	int tx = blockDim.x * blockIdx.x + threadIdx.x;
-	int ty = blockDim.y * blockIdx.y + threadIdx.y;
-	int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  int tx = blockDim.x * blockIdx.x + threadIdx.x;
+  int ty = blockDim.y * blockIdx.y + threadIdx.y;
+  int tz = blockDim.z * blockIdx.z + threadIdx.z;
 
-	if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
-		return;
+  if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
+    return;
 
-	// coordinates in the probe array
-	int ix = tx % Nprb;
-	int iy = tx / Nprb;
-	// closest integers for scan positions
-	int stx = roundf(scanx[ty + tz * Nscan]);
-	int sty = roundf(scany[ty + tz * Nscan]);
-	// skip scans where the probe position is negative (undefined)
-	if (stx < 0 || sty < 0)
-		return;
-	// shift in the object array to the starting point of probe multiplication
-	int shift = (Ndety - Nprb) / 2 * Ndetx + (Ndetx - Nprb) / 2;
+  // coordinates in the probe array
+  int ix = tx % Nprb;
+  int iy = tx / Nprb;
+  // closest integers for scan positions
+  int stx = roundf(scanx[ty + tz * Nscan]);
+  int sty = roundf(scany[ty + tz * Nscan]);
+  // skip scans where the probe position is negative (undefined)
+  if (stx < 0 || sty < 0)
+    return;
 
-	float2 g0 = g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan]; // data voxel for multiplication
-	// multilication in complex variables with simultaneous writing to the object array
-	atomicAdd(&f[(stx + ix) + (sty + iy) * N + tz * Nz * N].x, g0.x);
-	atomicAdd(&f[(stx + ix) + (sty + iy) * N + tz * Nz * N].y, g0.y);
+  // coordinates in the object array
+  size_t shift = (
+    // shift in the object array to the starting point of probe multiplication
+    + (Ndety - Nprb) / 2 * Ndetx
+    + (Ndetx - Nprb) / 2
+    // shift in the object array multilication for this thread
+    + ix
+    + iy * Ndetx
+    + ty * Ndetx * Ndety
+    + tz * Ndetx * Ndety * Nscan
+  );
+  float2 g0 = g[shift]; // data voxel for multiplication
+
+  // multilication in complex variables with simultaneous writing to the object array
+  atomicAdd(&f[(stx + ix) + (sty + iy) * N + tz * Nz * N].x, g0.x);
+  atomicAdd(&f[(stx + ix) + (sty + iy) * N + tz * Nz * N].y, g0.y);
 }
 
 // simultaneous writing to the probe array
@@ -154,29 +220,38 @@ void __global__ setpartprobe(float2 *prb, float2 *g, float2 *f, float *scanx, fl
 							 int Ntheta, int Nz, int N, int Nscan, int Nprb, int Ndetx, int Ndety)
 
 {
-	int tx = blockDim.x * blockIdx.x + threadIdx.x;
-	int ty = blockDim.y * blockIdx.y + threadIdx.y;
-	int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  int tx = blockDim.x * blockIdx.x + threadIdx.x;
+  int ty = blockDim.y * blockIdx.y + threadIdx.y;
+  int tz = blockDim.z * blockIdx.z + threadIdx.z;
+  if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
+    return;
 
-	if (tx >= Nprb * Nprb || ty >= Nscan || tz >= Ntheta)
-		return;
+  // coordinates in the probe array
+  int ix = tx % Nprb;
+  int iy = tx / Nprb;
+  // closest integers for scan positions
+  int stx = roundf(scanx[ty + tz * Nscan]);
+  int sty = roundf(scany[ty + tz * Nscan]);
+  // skip scans where the probe position is negative (undefined)
+  if (stx < 0 || sty < 0)
+    return;
 
-	// coordinates in the probe array
-	int ix = tx % Nprb;
-	int iy = tx / Nprb;
-	// closest integers for scan positions
-	int stx = roundf(scanx[ty + tz * Nscan]);
-	int sty = roundf(scany[ty + tz * Nscan]);
-	// skip scans where the probe position is negative (undefined)
-	if (stx < 0 || sty < 0)
-		return;
-	// shift in the object array to the starting point of probe multiplication
-	int shift = (Ndety - Nprb) / 2 * Ndetx + (Ndetx - Nprb) / 2;
+  // coordinates in the object array
+  size_t shift = (
+    // shift in the object array to the starting point of probe multiplication
+    + (Ndety - Nprb) / 2 * Ndetx
+    + (Ndetx - Nprb) / 2
+    // shift in the object array multilication for this thread
+    + ix
+    + iy * Ndetx
+    + ty * Ndetx * Ndety
+    + tz * Ndetx * Ndety * Nscan
+  );
+  float2 g0 = g[shift]; // data voxel for multiplication
 
-	float2 g0 = g[shift + ix + iy * Ndetx + ty * Ndetx * Ndety + tz * Ndetx * Ndety * Nscan]; // data voxel for multiplication
-	// multilication in complex variables with simultaneous writing to the object array
-	atomicAdd(&prb[ix + iy * Nprb + tz * Nprb * Nprb].x, g0.x);
-	atomicAdd(&prb[ix + iy * Nprb + tz * Nprb * Nprb].y, g0.y);
+  // multilication in complex variables with simultaneous writing to the object array
+  atomicAdd(&prb[ix + iy * Nprb + tz * Nprb * Nprb].x, g0.x);
+  atomicAdd(&prb[ix + iy * Nprb + tz * Nprb * Nprb].y, g0.y);
 }
 
 // Compute exp(1j * dir * dx), exp(1j * dir * dy) where dx, dy are in (-1, 1)
